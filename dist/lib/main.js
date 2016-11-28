d0f06fcc10da15c85ba4628cedf046cde789c6c5
supasak <supasak@wearyouwant.com>
2016-11-28 14:46:07 +0700

    split project

--- dist/lib/main.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-var Point_1 = require("./Point");
-exports.Point = Point_1.Point;
-var Stroke_1 = require("./Stroke");
-exports.Stroke = Stroke_1.Stroke;
-var Doodle_1 = require("./Doodle");
-exports.Doodle = Doodle_1.Doodle;
